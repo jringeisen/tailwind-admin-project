be1bd41bd61f3148d17f9214ba78f30b4ed54567
Jonathon Ringeisen <ringeisen85@gmail.com>
2019-08-10 20:53:58 -1000

    Fixed the margins on the navbar list items to extend the full ul element.

--- resources/js/partials/SidebarNavigation.vue
+++ resources/js/partials/SidebarNavigation.vue
@@ -2,8 +2,8 @@
 <transition name="slide-fade">
     <div class="left-side-nav" v-if="toggleLeftSidebar">
         <div class="p-5">
-            <ul class="text-white text-xs font-light">
-                <router-link :to="route" tag="li" class="p-2 cursor-pointer" active-class="active" v-for="(route, index) in routes" :key="index">
+            <ul class="text-white text-xs font-light -ml-5 -mr-5">
+                <router-link :to="route" tag="li" class="p-2 cursor-pointer pl-6" active-class="active" v-for="(route, index) in routes" :key="index">
                     <i :class="route.icon+' float-left mr-3 w-3 pt-1 leading-loose'"></i> 
                     {{ route.title }} 
                 </router-link>
