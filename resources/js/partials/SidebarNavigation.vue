ed31cb60ba13dfefec6b299b82602d09c0ef2f75
Jonathon Ringeisen <ringeisen85@gmail.com>
2019-08-10 15:35:27 -1000

    Added vue router.

--- resources/js/partials/SidebarNavigation.vue
+++ resources/js/partials/SidebarNavigation.vue
@@ -3,18 +3,11 @@
     <div class="left-side-nav" v-if="toggleLeftSidebar">
         <div class="p-5">
             <ul class="text-white text-xs font-light">
-                <li class="p-2 cursor-pointer hover:bg-gray-800" :class="{ 'bg-gray-800': item.show }" v-for="(item, index) in menu.parent" :key="index">
-                    <div @click.prevent="item.show = !item.show" class="hover:font-medium">
-                        <i :class="item.icon+' float-left mr-3 w-3 pt-1 leading-loose'"></i> 
-                        {{ item.name }} 
-                        <i v-if="item.child && !item.show" class="fas fa-caret-right float-right pt-1"></i>
-                        <i v-if="item.child && item.show" class="fas fa-caret-down float-right pt-1"></i>
-                    </div>
-                    <transition name="slide-fade">
-                    <ul class="ml-6" v-if="item.child && item.show">
-                        <li class="p-1 hover:font-medium" v-for="(child, index) in item.child" :key="index">- {{ child }}</li>
-                    </ul>
-                    </transition>
+                <li class="p-2 cursor-pointer" v-for="(route, index) in routes" :key="index">
+                    <router-link :to="route" active-class="active">
+                        <i :class="route.icon+' float-left mr-3 w-3 pt-1 leading-loose'"></i> 
+                        {{ route.title }} 
+                    </router-link>
                 </li>
             </ul>
         </div>
@@ -27,48 +20,21 @@ export default {
     data () {
         return {
             toggleLeftSidebar: true,
-            menu: {
-                parent: [
-                    {
-                        name: 'Dashboard',
-                        icon: 'fa fa-user',
-                        show: false
-                    },
-                    {
-                        name: 'Admin',
-                        icon: 'fa fa-unlock-alt',
-                        show: false
-                    },
-                    {
-                        name: 'Business',
-                        icon: 'fa fa-briefcase',
-                        show: false,
-                        child: [
-                            'Mileage',
-                            'Packages',
-                            'Events'
-                        ]
-                    },
-                    {
-                        name: 'Finances',
-                        icon: 'fa fa-money-bill-alt',
-                        show: false,
-                        child: [
-                            'Incomes',
-                            'Expenses'
-                        ]
-                    },
-                    {
-                        name: 'Clients',
-                        icon: 'fa fa-users',
-                        show: false,
-                        child: [
-                            'View Clients',
-                            'Potential Clients'
-                        ]
-                    }
-                ]
+        }
+    },
+    computed: {
+        routes: function () {
+            var routes = [];
+            for (var i in this.$router.options.routes) {
+                if (!this.$router.options.routes.hasOwnProperty(i)){
+                    continue
+                }
+                var route = this.$router.options.routes[i];
+                if(route.hasOwnProperty('title')) {
+                    routes.push(route);
+                }
             }
+            return routes;
         }
     },
     mounted () {
