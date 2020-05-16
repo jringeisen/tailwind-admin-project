cd1e4459820c4eb83ccd3e6032643d4f233e2072
Jonathon Ringeisen <ringeisen85@gmail.com>
2020-05-16 09:12:52 -0400

    Removed the header from the calendar.

--- public/js/app.js
+++ public/js/app.js
@@ -87,9 +87,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
-//
 /* harmony default export */ __webpack_exports__["default"] = ({});
 
 /***/ }),
@@ -801,24 +798,13 @@ var render = function() {
   var _c = _vm._self._c || _h
   return _c("div", [
     _c("div", { staticClass: "container mx-auto xl:flex" }, [
-      _c("div", { staticClass: "gray-card w-full" }, [
-        _vm._m(0),
-        _vm._v(" "),
+      _c("div", { staticClass: "gray-card w-full shadow-lg" }, [
         _c("div", { staticClass: "card-body" }, [_c("calendar")], 1)
       ])
     ])
   ])
 }
-var staticRenderFns = [
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "card-header" }, [
-      _c("p", [_vm._v("Calendar")])
-    ])
-  }
-]
+var staticRenderFns = []
 render._withStripped = true
 
 
--- resources/js/pages/Calendar.vue
+++ resources/js/pages/Calendar.vue
@@ -1,10 +1,7 @@
 <template>
     <div>        
         <div class="container mx-auto xl:flex">
-            <div class="gray-card w-full">
-                <div class="card-header">
-                    <p>Calendar</p>
-                </div>
+            <div class="gray-card w-full shadow-lg">
                 <div class="card-body">
                     <calendar />
                 </div>
